88e7e065ade7444acc60f5d650e0a8fe04a2c4e2
Демон Чайный <volshebnuyoleg666@yandex.ru>
2024-06-14 21:16:53 +0400

    Добавьте файлы проекта.

---
+++ Flipper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flipper(343434);
+        }
+
+        static void Flipper(double number)
+        {
+            double flipNumber = 0;
+            double divider = 1;
+            while (number % divider != number)
+            {
+                flipNumber = (flipNumber * 10) + ((number % (divider * 10)) - (number % divider)) / divider;
+                divider *= 10;
+            }
+            Console.WriteLine(flipNumber);
+        }
+    }
+}
